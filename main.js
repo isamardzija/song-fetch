acdbca9b3330a389d85d282f3717aa751103fd6e
Ivan <ivansam382@gmail.com>
2023-02-02 08:08:44 +0100

    looks

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
-const searchField = document.querySelector("#searchField");
-const resultsContainer = document.querySelector("#resultsContainer");
-const loading = document.querySelector("#loading");
-const errorMessage = document.querySelector("#errorMessage");
-let timeout;
-
 searchField.addEventListener("input", potrazi);
 
 function potrazi() {
+  let timeout;
   clearTimeout(timeout);
+
+  // ovaj setTimeout dodaje 0.5s na loading, čisto kako bi se loading ikona stigla vidjeti,
+  //moguće ga je maknuti, ali fetch onda zna biti pre brz pa se loading vidi vrlo kratko
   timeout = setTimeout(() => dohvati(), 1000);
 }
 
 async function dohvati() {
+  // selektori
+  const searchField = document.querySelector("#searchField");
+  const resultsContainer = document.querySelector("#resultsContainer");
+  const loading = document.querySelector("#loading");
+  const errorMessage = document.querySelector("#errorMessage");
+  let input = searchField.value;
+
+  // loading i brisanje postojanih rezultata/grešaka
   errorMessage.classList.add("hide");
   resultsContainer.innerHTML = "";
-  loading.classList.toggle("hide");
+  loading.classList.remove("hide");
 
-  let input = searchField.value;
-  const endpoint = `https://itunes.apple.com/search?term=${input}`;
-  let response = await fetch(endpoint);
+  //fetch
+  let response = await fetch(`https://itunes.apple.com/search?term=${input}`);
   let data = await response.json();
 
   setTimeout(() => {
+    // prikazi gresku ako nema rezultata
     if (data.resultCount === 0) {
       loading.classList.toggle("hide");
       errorMessage.classList.remove("hide");
+
+      // izlistavanje rezultata
     } else {
-      loading.classList.toggle("hide");
+      loading.classList.add("hide");
       data.results.forEach((song, i) => {
+        // ovaj setTimeout samo daje efekat da se pjesme na DOMU dodaju jedna za drugom, a ne sve istovremeno
+        // također neobvezan
         setTimeout(() => {
           let li = document.createElement("li");
           let span = document.createElement("span");
